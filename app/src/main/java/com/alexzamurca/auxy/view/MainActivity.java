cfc2b7b76db6399498317f93238427fa354f0585
alexzamurca <alexzamurca@yahoo.co.uk>
2021-02-13 22:57:49 +0000

    Bottom navigation view is now functional.

--- app/src/main/java/com/alexzamurca/auxy/view/MainActivity.java
+++ app/src/main/java/com/alexzamurca/auxy/view/MainActivity.java
@@ -3,11 +3,15 @@ package com.alexzamurca.auxy.view;
 import android.os.Bundle;
 
 import com.alexzamurca.auxy.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.View;
 
@@ -20,5 +24,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initBottomNavigationView();
+    }
+
+    private void initBottomNavigationView()
+    {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
