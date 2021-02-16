f7950ba37f30a6dd86e1fc5b39a21f49156ba290
JulienT <julientapon@gmail.com>
2021-02-16 16:54:08 +0000

    chatbotproto comm2

--- app/src/main/java/com/alexzamurca/auxy/model/ChatBotProto.java
+++ app/src/main/java/com/alexzamurca/auxy/model/ChatBotProto.java
@@ -1,44 +1,26 @@
 package com.alexzamurca.auxy.model;
+import android.speech.tts.TextToSpeech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Locale;
-import javax.speech.Central;
-import javax.speech.synthesis.Synthesizer;
-import javax.speech.synthesis.SynthesizerModeDesc;
 
-public class ChatBotProto {
+public class ChatBotProto extends AppCompatActivity {
     protected String[] replies = {"Stay calm, keep walking briskly to show you're going somewhere", "If a potential assailant starts to get closer behind you, think about crossing the road or turning towards a busier area"};
-
+    private TextToSpeech textToSpeech;
 
     public void initCall() {
-        try {
-            // Set property as Kevin Dictionary
-            System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us" + ".cmu_us_kal.KevinVoiceDirectory");
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i!=TextToSpeech.ERROR){
+                    textToSpeech.setLanguage(Locale.UK);
+                }
+            }
+        });
 
-            // Register Engine
-            Central.registerEngineCentral("com.sun.speech.freetts" + ".jsapi.FreeTTSEngineCentral");
+        textToSpeech.speak(replies[1], TextToSpeech.QUEUE_FLUSH, null, "request1");
 
-            // Create a Synthesizer
-            Synthesizer synthesizer
-                    = Central.createSynthesizer(
-                    new SynthesizerModeDesc(Locale.US));
-
-            // Allocate synthesizer
-            synthesizer.allocate();
-
-            // Resume Synthesizer
-            synthesizer.resume();
-
-            // Speaks the given text
-            // until the queue is empty.
-            synthesizer.speakPlainText(
-                    "Hello", null);
-            synthesizer.waitEngineState(
-                    Synthesizer.QUEUE_EMPTY);
-
-            // Deallocate the Synthesizer.
-            synthesizer.deallocate();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
     }
+
 }
