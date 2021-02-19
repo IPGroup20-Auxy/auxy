d9d6c324536edd58c295a725577ec1c9f69036bf
Sanskar <sanskar13@yahoo.co.uk>
2021-02-19 15:27:20 +0000

    Call fragment

--- app/src/main/java/com/alexzamurca/auxy/view/CallFragment.java
+++ app/src/main/java/com/alexzamurca/auxy/view/CallFragment.java
@@ -1,13 +1,18 @@
 package com.alexzamurca.auxy.view;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.alexzamurca.auxy.R;
+import com.google.android.material.snackbar.Snackbar;
 
 public class CallFragment extends Fragment {
 
@@ -17,5 +22,24 @@ public class CallFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_call, container, false);
+    }
+
+    private void initOnClickListeners(View view){
+
+        //Getting event listeners for corresponding ids
+        Button endButton = view.findViewById(R.id.EndCall);
+
+        LinearLayout switchChatBot = view.findViewById(R.id.SwitchChatBot);
+        LinearLayout recordAudio = view.findViewById(R.id.RecordAudio);
+        LinearLayout speaker = view.findViewById(R.id.Speaker);
+        LinearLayout EmergencyContact = view.findViewById(R.id.EmergencyContact);
+        LinearLayout DialPad = view.findViewById(R.id.DialPad);
+
+
+        endButton.setOnClickListener(chatBotView ->
+        {
+            Log.d("Onclick", "Hello world");
+        });
+
     }
 }
