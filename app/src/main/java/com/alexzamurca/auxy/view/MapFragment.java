b314b8243f13b9d999e116339fedee311eb1edc7
chibeze01 <chibeze2@hotmail.co.uk>
2021-02-24 19:06:52 +0000

    For this commit I have implemented the map object there are some things I had to do like installing google-play-services-map. I have, all together modified 5 files. The API_Key has been left in google_map_api.xml as opposed to it living in my personal gradle.local_properties. and i have made a connection from googel_map_ap.xml to AndroidManifest.xml, to maintain the Api_key in one location.

--- app/src/main/java/com/alexzamurca/auxy/view/MapFragment.java
+++ app/src/main/java/com/alexzamurca/auxy/view/MapFragment.java
@@ -1,22 +1,53 @@
 package com.alexzamurca.auxy.view;
+import com.alexzamurca.auxy.R;
+
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.alexzamurca.auxy.R;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync( this);
+        return rootView;
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * I just add a marker near Sydney, Australia.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        // set the type of map.
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        // Add a marker in Sydney and move the camera
+        LatLng Bath = new LatLng(51.380001f, -2.36f);
+        mMap.addMarker(new MarkerOptions()
+                .position(Bath)
+                .title("Marker in Bath"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(Bath, 17.0f));
     }
 }
+
